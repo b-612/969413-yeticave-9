2c74cf04c039b5b4fd2b508cea00152ac6c5c55c
Alexandr Kononov <fromtheorion@yandex.ru>
2019-04-20 00:05:02 +0300

    Задание 7. Шаблонизация проекта выполнено (не без боя) :) Игорь, спасибо за поддержку.

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+function formatting_amount (int $price): string
+{
+    if ($price < 1000) {
+        $result = $price;
+    } else {
+        $result = number_format($price, 0, ',', ' ');
+    }
+    return $result . ' <b class="rub">р</b>';
+}
